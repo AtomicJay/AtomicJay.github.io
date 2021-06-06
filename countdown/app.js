2ee309087ad41f5c90c9b0807d1cb7cd596da21c
AtomicJay <84993864+AtomicJay@users.noreply.github.com>
2021-06-05 18:45:07 -0700

    Remove const days

--- countdown/app.js
+++ countdown/app.js
@@ -17,7 +17,6 @@ function dateDiffFormatted (a, b) {
   const utc1 = Date.UTC(a.getUTCFullYear(), a.getUTCMonth(), a.getUTCDate(), a.getUTCHours(), a.getUTCMinutes(), a.getUTCSeconds(), a.getUTCMilliseconds())
   const utc2 = Date.UTC(b.getUTCFullYear(), b.getUTCMonth(), b.getUTCDate(), b.getUTCHours(), b.getUTCMinutes(), b.getUTCSeconds(), b.getUTCMilliseconds())
 
-  const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24))
   const hours = Math.floor((utc2 - utc1) / (1000 * 60 * 60))
   const minutes = Math.floor((utc2 - utc1) / (1000 * 60))
   const seconds = Math.floor((utc2 - utc1) / 1000)
