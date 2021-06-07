0d4a6dbd6fc3fc779bd03e53af4be903cb4788dd
AtomicJay <84993864+AtomicJay@users.noreply.github.com>
2021-06-07 15:55:50 -0700

    Add days to countdown, change color, change font, and change countdown date

--- countdown/app.js
+++ countdown/app.js
@@ -7,7 +7,7 @@ let selectElement
 
 function updateTime () {
   today = new Date()
-  theDay = new Date('2021-06-06T04:30:00Z')
+  theDay = new Date('2021-06-22T01:21:00Z')
   formatDate = dateDiffFormatted(today, theDay)
   selectElement = document.getElementById('timer')
   selectElement.innerHTML = formatDate
@@ -17,11 +17,13 @@ function dateDiffFormatted (a, b) {
   const utc1 = Date.UTC(a.getUTCFullYear(), a.getUTCMonth(), a.getUTCDate(), a.getUTCHours(), a.getUTCMinutes(), a.getUTCSeconds(), a.getUTCMilliseconds())
   const utc2 = Date.UTC(b.getUTCFullYear(), b.getUTCMonth(), b.getUTCDate(), b.getUTCHours(), b.getUTCMinutes(), b.getUTCSeconds(), b.getUTCMilliseconds())
 
+  const days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24))
   const hours = Math.floor((utc2 - utc1) / (1000 * 60 * 60))
   const minutes = Math.floor((utc2 - utc1) / (1000 * 60))
   const seconds = Math.floor((utc2 - utc1) / 1000)
   const milliseconds = utc2 - utc1
-  return String(hours % 24).padStart(2, '0') + ':' +
+  return String(days) + ':' +
+         String(hours % 24).padStart(2, '0') + ':' +
          String(minutes % 60).padStart(2, '0') + ':' +
          String(seconds % 60).padStart(2, '0') + ':' +
          String(milliseconds % 1000).padStart(3, '0')
